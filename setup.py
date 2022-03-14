336ca140fc25e45708886a4dfc045b3aee02ec11
sear9487github <79401401+sear9487github@users.noreply.github.com>
2022-03-14 18:12:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='Distutils',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='gward@python.net',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
-     )
+setup(
+    name='SearExt',
+    version='0.1.0',
+)
